f978539b3bd0c34f12c5c0042fa967647809fba8
Gabriel Calderon <garry019@gmail.com>
2026-09-21 15:41:17 -0500

    Actualizacion Template

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -7,9 +7,9 @@ export function Contacto() {
                     <h2 className="fw-bold mb-3">¿Tenés una idea en mente?</h2>
                     <a href="mailto:garry019@gmail.com" className="btn btn-dark px-4 fs-1">LET'S BUILD</a>
                 </div>
-                <div className="col-lg-8 mt-5">
+                <div className="col-lg-12 text-center" style={{marginTop: 200}}>
                     <h5 className="fw-bold mb-3" style={{ lineHeight: 0.5 }}>Gabriel Calderón</h5>
-                    <p className="text-secondary" style={{ lineHeight: 0 }}>Full Stack Developer & UX/UI · Bogotá, Colombia.</p>
+                    <p className="text-secondary mb-4" style={{ lineHeight: 0 }}>Full Stack Developer & UX/UI · Bogotá, Colombia.</p>
                     <a className="btn btn-sm btn-outline-dark me-1" target="_blank" href="https://github.com/garry019">GitHub</a>
                     <a className="btn btn-sm btn-outline-dark me-1" target="_blank" href="https://www.linkedin.com/in/gary-full-stack-web-developer/">LinkedIn</a>
                     <a className="btn btn-sm btn-outline-dark me-1" target="_blank" href="https://wa.me/5713026684002">WhatsApp</a>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -64,7 +64,7 @@ export function Home() {
                                 <div className="display-2 fw-bold lh-1">
                                     +{anios}<span className="text-secondary"></span>
                                 </div>
-                                <div className="text-secondary">Años de experiencia en desarrollo</div>
+                                <div className="text-secondary">Años de experiencia</div>
                             </div>
                         </div>
                     </div>
